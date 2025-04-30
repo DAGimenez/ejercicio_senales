f510a090fd5d67a0adbf32f9a85f5ef250a374d5
DAGimenez <gimenezdylan753@gmail.com>
2025-04-30 12:45:19 -0300

    7/ connect

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -3,11 +3,15 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
+@onready var red_generator = $RedGenerator
+@onready var violet_generator = $VioletGenerator
 
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	red_generator.connect("button_down",count_new_instance)
+	violet_generator.connect("button_down",count_new_instance)
 	pass
 
 func count_new_instance ():
